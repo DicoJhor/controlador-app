b0ff85fa526fa40d9a8ea53c5c324ac5f8886516
Keysls <keysls.de@gmail.com>
2026-03-16 17:38:52 -0500

    fix cors for production

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express()
 
 // CORS configurado para frontend (Vite)
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
   credentials: true
 }))
 
