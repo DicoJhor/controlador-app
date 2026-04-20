910263abbda28ea1878916e0c557dedd4cbc2f3c
Keysls <keysls.de@gmail.com>
2026-04-20 15:52:43 -0500

    fix: corregir renderizado de 0 suelto en columna stock

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/admin/', // ← cambiar './' por la ruta real
+  base: '/', // ← cambiar './' por la ruta real
 
   plugins: [
     react(),
